6cafacc2ade5dc906fc10c7d7a5763633d8f701d
Vijay Pratap <lit2019015@iiitl.ac.in>
2022-11-03 00:31:32 +0530

    first commit

---
+++ home.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+	
+    <img class="wave" src="assets/img/wave.png">
+    <?php include 'navbar.php' ?>
+    <?php include 'topbar.php' ?>
+	<div class="container">
+		<div class="img">
+			<img src="assets/img/bg.svg">
+		</div>
+		
+        
+		
+    </div>
+</body>
+</html>
